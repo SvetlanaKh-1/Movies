24041aeabb81a2cba3c74fe5e06de47ed57a6f6c
Svetlana Khlystova <khlystova_sv@bk.ru>
2025-01-27 20:19:36 +0300

    Add tests for findLast

--- src/test/java/ru/netology/MovieManager/managers/MovieManagerTest.java
+++ src/test/java/ru/netology/MovieManager/managers/MovieManagerTest.java
@@ -53,6 +53,22 @@ public class MovieManagerTest {
     }
 
     @Test
+    public void shouldFindLastAboveLimit() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+
+        String[] expected = {"Film 6", "Film 5", "Film 4", "Film 3", "Film 2"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void shouldFindLastBelowLimit() {
         MovieManager manager = new MovieManager();
 
@@ -66,7 +82,7 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void shouldFindLastAboveLimit() {
+    public void shouldFindLastLimitWithParams() {
         MovieManager manager = new MovieManager(6);
 
         manager.addMovie("Film 1");
@@ -77,6 +93,38 @@ public class MovieManagerTest {
         manager.addMovie("Film 6");
 
         String[] expected = {"Film 6", "Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastAboveLimitWithParams() {
+        MovieManager manager = new MovieManager(6);
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+        manager.addMovie("Film 7");
+
+        String[] expected = {"Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastBelowLimitWithParams() {
+        MovieManager manager = new MovieManager(6);
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
